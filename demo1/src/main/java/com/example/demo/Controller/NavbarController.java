584f65d670985083e57cee9976e2fe2ea5f696d5
May3012 <139161004+May3012@users.noreply.github.com>
2024-04-05 16:18:47 -0500

    commit olvidado - todo lo de local

--- demo1/src/main/java/com/example/demo/Controller/NavbarController.java
+++ demo1/src/main/java/com/example/demo/Controller/NavbarController.java
@@ -2,11 +2,16 @@ package com.example.demo.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class NavbarController {
     @RequestMapping(value="/principal",method= RequestMethod.GET)
-    public String paginaPrincipal(){
+    public String paginaHome(@RequestParam(name = "code",required = false) String code) {
+        if (code !=null && code.equals("PUCP")) {
+            return "principal2";
+        }
         return "principal";
     }
 }
+
