ca03dad6aeec7a30bb945cbdbd05ff95df922970
May3012 <139161004+May3012@users.noreply.github.com>
2024-04-05 15:12:10 -0500

    Se creó un html que incluye el logo en html y se modificó el NavbarController

--- src/main/java/com/example/lab1/Controller/NavbarController.java
+++ src/main/java/com/example/lab1/Controller/NavbarController.java
@@ -8,7 +8,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class NavbarController {
     @RequestMapping(value="/principal",method= RequestMethod.GET)
-    public String paginaHome(){
-            return "principal";
+    public String paginaHome(@RequestParam(name = "code",required = false) String code) {
+        if (code !=null && code.equals("PUCP")) {
+            return "principal2";
         }
+        return "principal";
+    }
 }
