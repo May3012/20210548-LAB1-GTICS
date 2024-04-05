17309b13d58714db764e78733126b1608792bac3
May3012 <139161004+May3012@users.noreply.github.com>
2024-04-04 21:00:48 -0500

    Navbar Controller

--- demo1/src/main/java/com/example/demo/Controller/NavbarController.java
+++ demo1/src/main/java/com/example/demo/Controller/NavbarController.java
@@ -1,1 +1,12 @@
 package com.example.demo.Controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class NavbarController {
+    @RequestMapping(value="paginaprincipal",method= RequestMethod.GET)
+    public String paginaPrincipal(){
+        return "principal";
+    }
+}
